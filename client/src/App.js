0cf006e33a89cdd0ab9fea3015ccbfbb3657fdc2
unholydeath785 <ivansonofcoul@gmail.com>
2018-07-13 00:14:20 -0700

    added basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
 import './App.css';
 
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+)
+
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+)
+
+const Topic = ({ match }) => (
+	<div>
+		<h3>{match.params.topicId}</h3>
+	</div>
+)
+  
+const Topics = ({ match }) => (
+	<div>
+		<h2>Topics</h2>
+		<ul>
+		<li>
+			<Link to={`${match.url}/rendering`}>
+			Rendering with React
+			</Link>
+		</li>
+		<li>
+			<Link to={`${match.url}/components`}>
+			Components
+			</Link>
+		</li>
+		<li>
+			<Link to={`${match.url}/props-v-state`}>
+			Props v. State
+			</Link>
+		</li>
+		</ul>
+
+		<Route path={`${match.path}/:topicId`} component={Topic}/>
+		<Route exact path={match.path} render={() => (
+		<h3>Please select a topic.</h3>
+		)}/>
+	</div>
+)
+
 class App extends Component {
-	state = { users: [] }
-
-	componentDidMount() {
-		fetch('/users')
-			.then(res => res.json())
-			.then(users => this.setState({ users }));
-	}
-
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-				<h1>Users</h1>
-				{this.state.users.map(user =>
-					<div key={user.id}>{user.username}</div>
-				)}
-			</div>
+			<Router>
+				<div>
+					<ul>
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/about">About</Link></li>
+						<li><Link to="/topics">Topics</Link></li>
+					</ul>
+					<hr/>
+					<Route exact path="/" component={Home}/>
+					<Route path="/about" component={About}/>
+					<Route path="/topics" component={Topics}/>
+				</div>
+			</Router>
 		);
 	}
 }
