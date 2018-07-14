1b950005b393c2b0b6d5a8587590fdf74227c9b0
unholydeath785 <ivansonofcoul@gmail.com>
2018-07-14 13:04:37 -0700

    put build behind a proxy

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api', indexRouter);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
