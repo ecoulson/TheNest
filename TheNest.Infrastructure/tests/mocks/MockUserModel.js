f1b4ab2418b8c1793513edb3e0a75ce00cc3c33f
Evan Coulson <evan.m.coulson@gmail.com>
2019-03-12 23:16:22 -0700

    refactored components of projects into seperate projects

---
+++ TheNest.Infrastructure/tests/mocks/MockUserModel.js
@@ -0,0 +1,21 @@
+const Mock = require('./Mock');
+
+class MockUserModel extends Mock {
+	findOne(query, callback) {
+		if (this.shouldThrow) {
+			return callback(new Error("Error should be thrown for testing"));
+		}
+		if (!query.microsoftID) {
+			callback(null, {})
+		}
+		if (query.microsoftID < 0) {
+			callback(null, null);
+		} else {
+			callback(null, {
+				id: query.microsoftID
+			});
+		}
+	}
+}
+
+module.exports = MockUserModel;
